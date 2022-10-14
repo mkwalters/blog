bbc04cd89b8b23c061322437e53a23859280312e
Mitchell <mkwsoftware@gmail.com>
2022-10-13 21:33:59 -0700

    change message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My name is Mitchell. I write software, play chess, read fantasy books and play guitar. Welcome to my page!
+          My name is Mitchell. I write software, play chess, chill with my girlfriend Adriana, read fantasy books and play guitar. Welcome to my blog!
         </p>
         <a
           className="App-link"
