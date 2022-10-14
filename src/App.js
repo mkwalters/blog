ee3473f0f10f897afbddf3ec8c697a357178f604
Mitchell <mkwsoftware@gmail.com>
2022-10-13 20:54:33 -0700

    introduction

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is Mitchell. I write software, play chess, read fantasy books and play guitar. Welcome to my page!
         </p>
         <a
           className="App-link"
