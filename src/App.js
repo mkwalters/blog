1e669e73418568a7ad67dbaaa234a0e726cf1e3b
Mitchell <mkwsoftware@gmail.com>
2022-10-13 22:13:09 -0700

    image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import logo from './portrait.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" />
         <p>
           My name is Mitchell. I write software, play chess, chill with my girlfriend Adriana, read fantasy books and play guitar. Welcome to my blog!
         </p>
